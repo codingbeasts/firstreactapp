3614c0ecf901b4525a97dfab555239444ad5f292
dipanshushora <deepanshushora@gmail.com>
2020-12-20 15:15:59 +0530

    created object and map func to iterate

--- src/js/app.js
+++ src/js/app.js
@@ -2,37 +2,74 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../css/styles.css';
-
-const movieInfo = {
-  movieName:["Men of Steel","Batman v Superman: Dawn of Justice","Wonder Woman","Justice League","Aquaman"],
-  movieImage:["https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRLtBapSgavJgGxTgMgC-Q-9DRivYiyOw9EXeFeX7iXiVvYytGz",
-  "https://upload.wikimedia.org/wikipedia/en/2/20/Batman_v_Superman_poster.jpg",
-  "https://images-na.ssl-images-amazon.com/images/I/91FsZrtVvDL._RI_.jpg",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAW8_E8ynNo97bf-PGda55Uruz9eCH1HP8gw&usqp=CAU",
-  "https://m.media-amazon.com/images/M/MV5BOTk5ODg0OTU5M15BMl5BanBnXkFtZTgwMDQ3MDY3NjM@._V1_.jpg"],
-  movieDirector:["Zack Snyder","Zack Snyder","Patty Jenkins","Zack Snyder","James Wan"],
-  movieReleaseDate:["14 June 2013","25 March 2016","15 May 2017","15 November 2017","14 December 2018"]
-};
+const movieInfo=[
+  {
+    id:1,
+    name:"Men of Steel",
+    image:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRLtBapSgavJgGxTgMgC-Q-9DRivYiyOw9EXeFeX7iXiVvYytGz",
+    alt:"steel",
+    director:"Zack Snyder",
+    releasedate:"14 June 2013"
+  },
+  {
+    id:2,
+    name:"Batman v Superman: Dawn of Justice",
+    image:"https://upload.wikimedia.org/wikipedia/en/2/20/Batman_v_Superman_poster.jpg",
+    alt:"dawn",
+    director:"Zack Snyder",
+    releasedate:"25 March 2016"
+  },
+  {
+    id:3,
+    name:"Wonder Woman",
+    image:"https://images-na.ssl-images-amazon.com/images/I/91FsZrtVvDL._RI_.jpg",
+    alt:"wonder",
+    director:"Patty Jenkins",
+    releasedate:"15 May 2017"
+  },
+  {
+    id:4,
+    name:"Justice League",
+    image:"https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/Justice-League-teaser-poster.jpg",
+    alt:"League",
+    director:"Zack Snyder",
+    releasedate:"15 November 2017"
+  },
+  {
+    id:5,
+    name:"Aquaman",
+    image:"https://m.media-amazon.com/images/M/MV5BOTk5ODg0OTU5M15BMl5BanBnXkFtZTgwMDQ3MDY3NjM@._V1_.jpg",
+    alt:"aqua",
+    director:"James Wan",
+    releasedate:"14 December 2018"
+  }
+];
 
 function DC(){
   return(
     <section className="movie">
-      <Movie mName={movieInfo.movieName[0]} mImg={movieInfo.movieImage[0]} mDir={movieInfo.movieDirector[0]} mRel={movieInfo.movieReleaseDate[0]} />
-      <Movie mName={movieInfo.movieName[1]} mImg={movieInfo.movieImage[1]} mDir={movieInfo.movieDirector[1]} mRel={movieInfo.movieReleaseDate[1]} />
-      <Movie mName={movieInfo.movieName[2]} mImg={movieInfo.movieImage[2]} mDir={movieInfo.movieDirector[2]} mRel={movieInfo.movieReleaseDate[2]} />
-      <Movie mName={movieInfo.movieName[3]} mImg={movieInfo.movieImage[3]} mDir={movieInfo.movieDirector[3]} mRel={movieInfo.movieReleaseDate[3]} />
-      <Movie mName={movieInfo.movieName[4]} mImg={movieInfo.movieImage[4]} mDir={movieInfo.movieDirector[4]} mRel={movieInfo.movieReleaseDate[4]} />
+      {
+        movieInfo.map((movie)=>{
+          return <Movie key={movie.id} {...movie} />; //--- spread operator
+          // return <Movie key={movie.id} movies={movie} />;
+        })
+      }
     </section>
   );
 }
 
-const Movie = (props) =>{
+const Movie = ({name,image,alt,director,releasedate}) =>{
+  // const {name,image,alt,director,releasedate} = props; without prop.Object or
+  /*
+    pass function parameters like const Movie = ({name,image,alt,director,releasedate}) =>{};
+  */
+  // const {name,image,alt,director,releasedate} = props.movies;
   return(
     <article className="movie_info">
-      <img src={props.mImg} alt="2527s_Justice_League"/>
-      <span>{props.mName}</span>
-      <h1>{props.mDir}</h1>
-      <span>{props.mRel}</span>
+      <img src={image} alt={alt}/>
+      <span>{name}</span>
+      <h1>{director}</h1>
+      <span>{releasedate}</span>
     </article>
   );
 };
